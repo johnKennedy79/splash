a6b42213ceab5ef717b5c6b713bfaabe8105354e
John Kennedy <john,m,r,kennedy.jk@gmail.com>
2024-07-04 00:47:00 +0100

    js enabeled

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let firstLine = "4 Wireframe Lane,";
+let town = "Sketchton,";
+let county = "Webfolk,";
+let postCode = "NR1 W00";
+let tel = "Tel: 0800 444 321";
+let email = "E-mail: hello@beavers.ltd";
+
+
+
+
+
+
+
+
+
+
